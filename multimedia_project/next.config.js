b5ca7efe8633da13b0f14d04693ba17f8c2280a6
JoaoCupido <jcupido13@gmail.com>
2022-10-20 17:58:34 +0100

    adição de páginas dinâmicas de cada animal corretamente implementado; atualização de data dos animais no .json; correção de config de tailwind e adição de propriedade "alt" to Image

--- multimedia_project/next.config.js
+++ multimedia_project/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
   reactStrictMode: true,
   swcMinify: true,
   images: {
---
+++ multimedia_project/pages/animals/[id].js
@@ -0,0 +1,81 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+import Footer from "../../comps/footer";
+import Header from "../../comps/header";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+// Fetching data from the JSON file
+import fsPromises from 'fs/promises';
+import path from 'path'
+export async function getStaticProps({params}) {
+    const filePath = path.join(process.cwd(), 'animalsData.json');
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData);
+
+    let singleAnimalData = {};
+
+    for (let i = 0; i < Object.keys(objectData.animals).length; i++)
+    {
+        if ((objectData.animals)[i].id === params.id) {
+            singleAnimalData = (objectData.animals)[i];
+            break;
+        }
+    }
+
+    return {
+        props: singleAnimalData
+    }
+}
+
+export async function getStaticPaths() {
+    const filePath = path.join(process.cwd(), 'allAnimals.json');
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData);
+
+    const paths = objectData.map(animal => {
+        return { params: { id: animal }}
+    })
+
+    return {
+        paths,
+        fallback: false
+    };
+}
+
+export default function Animal(props) {
+    const router = useRouter()
+    const { id } = router.query
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>List of Animals</title>
+                <meta name="description" content="A list of animals to search for and filter." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Header/>
+
+            <main>
+                <div className={styles.container}>
+                    <Head>
+                        <title>{props.name}</title>
+                    </Head>
+
+                    <main className={styles.main}>
+                        <h1 className={styles.title}>
+                            {id}
+                        </h1>
+
+                        <Image src={((props.images)[0]).imagelink} width="300px" height="300px" alt={((props.images)[0]).alt}/>
+
+                    </main>
+                </div>
+                )
+            </main>
+
+            <Footer/>
+        </div>
+    )
+}
