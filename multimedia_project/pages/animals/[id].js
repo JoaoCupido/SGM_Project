b05c1b2789d7b253a25b76371c5a3fe0d150b4ff
JoaoCupido <jcupido13@gmail.com>
2022-11-16 21:06:45 +0000

    POGGERS Possible Fix to 3D Models not loading correctly

--- multimedia_project/pages/animals/[id].js
+++ multimedia_project/pages/animals/[id].js
@@ -5,6 +5,7 @@ import React from "react";
 import {CubeTexture, SceneLoader} from "@babylonjs/core";
 import '@babylonjs/loaders/glTF';
 import BabylonScene from "../../comps/BabylonScene";
+import SceneComponent from 'babylonjs-hook';
 import {Carousel} from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -191,7 +192,8 @@ export default function Animal(props) {
                     </div>
                     <div className={`${"row"}`}>
                         <div className={`${"pb-3 pt-2 col d-flex align-items-center justify-content-center"}`}>
-                            <BabylonScene antialias adaptToDeviceRatio onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" className={`${"rounded"}`}/>
+                            {/*<BabylonScene antialias adaptToDeviceRatio onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" className={`${"rounded"}`}/> */}
+                            <SceneComponent antialias adaptToDeviceRatio onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" style={{ width: "100%", height: "100%" }} className={`${"rounded"}`}/>
                         </div>
                         { hasVideo(props.video, props.id) }
                     </div>
