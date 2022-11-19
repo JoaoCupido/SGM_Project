fb0da2ad5f14f9c0c55d83c424dab88c4583cc90
JoaoCupido <jcupido13@gmail.com>
2022-11-19 19:23:34 +0000

    remover middleware e usar a função rewrites para redirecionar página no next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,22 @@ const nextConfig = {
   images: {
     domains: ['api-ninjas.com', 'images.pexels.com', 'images.unsplash.com', 'https://joaocupido.github.io/sgm_project'],
     unoptimized: true
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/animals/rabbit',
+        destination: '/animals/bunny'
+      },
+      {
+        source: '/animals/ladybeetle',
+        destination: '/animals/ladybug'
+      },
+      {
+        source: '/animals/lady-beetle',
+        destination: '/animals/ladybug'
+      }
+    ];
   }
 }
 
