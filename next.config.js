0dd9a846d69a3797e21044c0471dceae691b4b85
JoaoCupido <jcupido13@gmail.com>
2022-11-27 11:50:07 +0000

    adicionado o dinossauro "Velociraptor"

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['api-ninjas.com', 'images.pexels.com', 'images.unsplash.com', 'https://joaocupido.github.io/sgm_project'],
+    domains: ['api-ninjas.com', 'images.pexels.com', 'images.unsplash.com', 'cdn.pixabay.com', 'https://joaocupido.github.io/sgm_project'],
     unoptimized: true
   }
 }
